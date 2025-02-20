aff0f4fd440cb32da60e0ad0747ecddf8da5089f
Karan-Bharti1 <karan.codes1@gmail.com>
2025-02-20 11:18:23 +0530

    feat: created a postBook function to add book data

--- src/features/books/booksSlicer.js
+++ src/features/books/booksSlicer.js
@@ -5,6 +5,15 @@ export const fetchBooks=createAsyncThunk("books/fetchBooks",async()=>{
     const response =await axios.get(bookUrl)
     return response.data
 })
+export const postBook=createAsyncThunk("books/postBooks",async(data)=>{
+    const response=await axios.post(bookUrl,data,{
+        method:'POST'
+        ,headers:{
+            'Content-Type':'application/json'
+        }
+    })
+    return response.data
+})
  export const booksSlicer=createSlice({
     name:"books",
     initialState:{
@@ -23,6 +32,17 @@ export const fetchBooks=createAsyncThunk("books/fetchBooks",async()=>{
             state.books=action.payload
         })
         builder.addCase(fetchBooks.rejected,(state,action)=>{
+            state.status="error",
+            state.error=action.payload.error
+        })
+        builder.addCase(postBook.pending,state=>{
+            state.status="loading"
+        })
+        builder.addCase(postBook.fulfilled,(state,action)=>{
+            state.status="succeeded",
+            state.books=state.books.filter(book=>book._id!=action.payload._id)   
+        })
+        builder.addCase(postBook.rejected,(state,action)=>{
             state.status="error",
             state.error=action.payload.error
         })
